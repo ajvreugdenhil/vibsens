c90e524bc2eb4f02ac76ad9c30d4228e068a2cae
Arjan <33868752+ajvreugdenhil@users.noreply.github.com>
2021-06-25 14:49:34 +0200

    First version firmware

---
+++ VibrationSensor/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LSM6.h>
+
+LSM6 imu;
+
+char report[80];
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("test???");
+  Wire.begin();
+
+  if (!imu.init())
+  {
+    Serial.println("Failed to detect and initialize IMU!");
+    while (1)
+      ;
+  }
+  imu.enableDefault();
+}
+
+int16_t previous_x = 0;
+int16_t previous_y = 0;
+int16_t previous_z = 0;
+
+void loop()
+{
+  delay(100);
+  imu.readAcc();
+  snprintf(report, sizeof(report), "[%d, %d, %d]",
+           imu.a.x - previous_x,
+           imu.a.y - previous_y,
+           imu.a.z - previous_z);
+  Serial.println(report);
+
+  previous_x = imu.a.x;
+  previous_y = imu.a.y;
+  previous_z = imu.a.z;
+}
