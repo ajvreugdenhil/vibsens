1ef716f7f6708908d359432c210f14d68dbf1ce1
Arjan <33868752+ajvreugdenhil@users.noreply.github.com>
2021-06-26 13:52:32 +0200

    moved analysis into microcontroller
    to allow for more data throughput, making the data way more useful

--- VibrationSensor/src/main.cpp
+++ VibrationSensor/src/main.cpp
@@ -4,7 +4,12 @@
 
 LSM6 imu;
 
-char report[80];
+char report[250];
+
+const int max_batchsize = 5000;
+int16_t history_x[max_batchsize];
+int16_t history_y[max_batchsize];
+int16_t history_z[max_batchsize];
 
 void setup()
 {
@@ -28,15 +33,62 @@ int16_t previous_z = 0;
 
 void loop()
 {
-  delay(10);
-  imu.readAcc();
-  snprintf(report, sizeof(report), "[%d, %d, %d]",
-           imu.a.x - previous_x,
-           imu.a.y - previous_y,
-           imu.a.z - previous_z);
-  Serial.println(report);
+  Serial.flush();
+  int batchsize = 0;
+  while (!Serial.available())
+  {
+    imu.readAcc();
+    history_x[batchsize] = imu.a.x - previous_x;
+    history_y[batchsize] = imu.a.y - previous_y;
+    history_z[batchsize] = imu.a.z - previous_z;
+    previous_x = imu.a.x;
+    previous_y = imu.a.y;
+    previous_z = imu.a.z;
+    batchsize += 1;
+    if (batchsize >= max_batchsize)
+    {
+      batchsize = 0;
+    }
+  }
+  while(Serial.available())
+  {
+    Serial.read();
+  }
 
-  previous_x = imu.a.x;
-  previous_y = imu.a.y;
-  previous_z = imu.a.z;
+  const int histories_count = 3;
+  int16_t *histories[histories_count] = {history_x, history_y, history_z};
+  int16_t max_amplitudes[3] = {0, 0, 0};
+  int16_t avg_amplitudes[3] = {0, 0, 0};
+  int16_t frequencies[3] =    {0, 0, 0};
+  for (int i = 0; i < histories_count; i++)
+  {
+    int16_t max_amplitude = 0;
+    int64_t total_amplitude = 0;
+    for (int j = 0; j < batchsize; j++)
+    {
+      if (abs(histories[i][j]) > abs(max_amplitude))
+      {
+        max_amplitude = histories[i][j];
+      }
+      total_amplitude += histories[i][j];
+
+      max_amplitudes[i] = max_amplitude;
+      avg_amplitudes[i] = total_amplitude / batchsize;
+    }
+  }
+
+  snprintf(report,
+           sizeof(report),
+           "{\"MA_X\":%d, \"MA_Y\":%d, \"MA_Z\":%d, \"AA_X\":%d, \"AA_Y\":%d, \"AA_Z\":%d, \"F_X\":%d, \"F_Y\":%d, \"F_Z\":%d, \"SC\":%d}",
+           max_amplitudes[0],
+           max_amplitudes[1],
+           max_amplitudes[2],
+           avg_amplitudes[0],
+           avg_amplitudes[1],
+           avg_amplitudes[2],
+           frequencies[0],
+           frequencies[1],
+           frequencies[2],
+           batchsize);
+  Serial.println(report);
 }
--- vibsens.chart.py
+++ vibsens.chart.py
@@ -2,6 +2,7 @@
 # Description: Custom vibration sensor plugin
 # Author: Bangedaon | Arjan Vreugdenhil
 
+from serial.serialutil import SerialException
 from bases.FrameworkServices.SimpleService import SimpleService
 import serial
 import json
@@ -53,109 +54,70 @@ class Service(SimpleService):
         self.order = ORDER
         self.definitions = CHARTS
         self.c = None
-        for i in range(64):
+        for i in range(1, 64):
             if self.c is None:
                 try:
                     port = '/dev/ttyUSB' + str(i)
-                    self.c = serial.Serial(port, baudRate, timeout=0.1)
+                    self.c = serial.Serial(port, baudRate, timeout=0.05)
                     # TODO: verify valid data coming from uart
                     self.debug(port + " is accessible")
                 except:
                     pass
-        self.queue_lock = threading.Lock()
-        self.queue = Queue.Queue()  # queue in python3
-        self.listener = serialListener(self.queue, self.queue_lock, self.c)
-        self.listener.start()
 
     def get_data(self):
         data = dict()
-        raw_data = None
-        self.queue_lock.acquire()
-        if self.queue.qsize() < 1:
-            return None
-
-        batch_data = []
-        while not self.queue.empty():
-            batch_data.append(self.queue.get())
-        self.queue_lock.release()
-
-        data["sample_count"] = len(batch_data)
-
-        data["max_amplitude_x"] = batch_data[0][0]
-        data["max_amplitude_y"] = batch_data[0][1]
-        data["max_amplitude_z"] = batch_data[0][2]
-        for d in batch_data:
-            if abs(d[0]) > abs(data["max_amplitude_x"]):
-                data["max_amplitude_x"] = d[0]
-            if abs(d[1]) > abs(data["max_amplitude_y"]):
-                data["max_amplitude_x"] = d[1]
-            if abs(d[2]) > abs(data["max_amplitude_z"]):
-                data["max_amplitude_x"] = d[2]
-
-        total_x = 0
-        total_y = 0
-        total_z = 0
-        for d in batch_data:
-            total_x += d[0]
-            total_y += d[1]
-            total_z += d[2]
-        data["amplitude_x"] = total_x / len(batch_data)
-        data["amplitude_y"] = total_y / len(batch_data)
-        data["amplitude_z"] = total_z / len(batch_data)
-
-        data['frequency_0'] = 12
-
+        
+        
+        input = self.receive_raw()
+        if input is None:
+            return data
+            
+        
+        if input is not None:
+            data["sample_count"] = input["SC"]
+            data["max_amplitude_x"] = input["MA_X"]
+            data["max_amplitude_y"] = input["MA_Y"]
+            data["max_amplitude_z"] = input["MA_Z"]
+            data["amplitude_x"] = input["AA_X"]
+            data["amplitude_y"] = input["AA_Y"]
+            data["amplitude_z"] = input["AA_Z"]
+            data['frequency_0'] = 12
         return data
 
     def check(self):
         return True
 
-
-class serialListener (threading.Thread):
-    def __init__(self, q, queueLock, c):
-        threading.Thread.__init__(self)
-        self.q = q
-        self.c = c
-        self.queueLock = queueLock
-        self.running = False
-
-    def run(self):
-        self.running = True
-        while (self.running):
-            data = self.receive_raw()
-            if data is not None:
-                self.queueLock.acquire()
-                self.q.put(data)
-                self.queueLock.release()
-
-    def exit(self):
-        self.running = False
-
     def receive_raw(self):
         if self.c is not None:
             try:
+                self.c.write(b'\n')
                 while self.c.in_waiting < 1:
                     pass
-                if self.c.read(1) is not b'[':
+                if self.c.read(1) is not b'{':
                     self.c.readline()
             except IOError:
+                self.error("IOError")
+                return None
+            except SerialException:
+                self.error("SerialException again")
                 return None
             response = ""
             try:
                 response = self.c.readline().decode("utf-8").strip()
-                # because we don't have a peek() function
-                response = '[' + response
+                if len(response) < 2:
+                    #self.error("line read but found unexpected size")
+                    return None
+                response = '{' + response # because we don't have a peek() function
             except:
+                #self.error("Could not read data")
                 return None
 
             try:
                 json_object = json.loads(response)
-                if len(json_object) == 3:
-                    return json_object
-                else:
-                    return None
+                return json_object
             except:
-                print("Json Parsing Failed: " + response)
+                #print("Json Parsing Failed: " + response)
                 return None
         else:
+            #self.error("Connection broken")
             return None
